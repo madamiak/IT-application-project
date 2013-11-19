e61f09ed63b120bc3fa2bbb8c23fe5897aca86e8
zdanowiczkonrad <zdanowiczkonrad@gmail.com>
2013-11-20 00:51:03 +0100

    Improvement in UI, autocompletion model, added firing event from search controller to main controller

--- ui/app/scripts/controllers/main.js
+++ ui/app/scripts/controllers/main.js
@@ -3,9 +3,16 @@
 
 angular.module('uiApp').controller('MainController', function ($scope) {
 		$scope.searchMessage='Type your destinations!';
-		$scope.explore = function($scope) {
-		}
-	});
+		
+		$scope.explore = function($scope) {}
+
+		$scope.$on('showMap', function(event,points) {
+			console.log("event happened.");
+			console.log(points);
+		
+		});
+	
+});
 
 angular.module('uiApp').directive('searchForm', function() {
 	return {
