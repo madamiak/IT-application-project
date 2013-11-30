157391e6939bbbf35e2efac00e0eff8edba7a92a
rav-90 <rwawszczak@gmail.com>
2013-11-30 07:43:37 -0800

    Added point type POI with id 3

--- TS - database/ts.sql
+++ TS - database/ts.sql
@@ -1232,6 +1232,8 @@ INSERT INTO `point_type` (`idpoint_type`, `type_category`) VALUES
 (1, 'Hotel');
 INSERT INTO `point_type` (`idpoint_type`, `type_category`) VALUES
 (2, 'Destination');
+INSERT INTO `point_type` (`idpoint_type`, `type_category`) VALUES
+(3, 'POI');
 
 -- --------------------------------------------------------
 
