ecb3b53eed14c3f2166173c82cba33676dd0cebd
zdanowiczkonrad <zdanowiczkonrad@gmail.com>
2014-01-22 01:06:23 +0100

    Implemented authorisation

--- ui/app/scripts/controllers/main.js
+++ ui/app/scripts/controllers/main.js
@@ -3,18 +3,36 @@
 
 angular.module('uiApp').controller('MainController', function ($scope,$http) {
 
-		$scope.explore = function($scope) {};
-		$scope.loginStatus = "You are not logged in.";
-  		$scope.username="";
-  		$scope.password="";
   		$scope.endpoint='http://localhost:9000';
+		
+		//add to local storage if needed
+		$scope.loggedIn = false;
+		$scope.notLoggedMessage = "You are not logged in.";
+		$scope.loggedMessage = "Hello, ";
 
+  		$scope.username;
+  		$scope.password;
+  		
+  		$scope.showMessage = function(message) {alert(message);}
+
+  		$scope.tryLogout = function() {
+  			$scope.loggedIn=false;
+  		}
   		$scope.tryLogin = function() {
   			console.log($scope.username+" "+$scope.password);
   			var reqUrl = $scope.endpoint+"/user/authenticate";
-  			$http({method: 'POST', url: reqUrl}).
+  			$http({method: 'POST', url: reqUrl,data: {username: $scope.username,password: $scope.password}}).
 			  success(function(data, status, headers, config) {
-			  console.log(data);
+			  	console.log(data);
+			  	if(data.code=="UNAUTHORIZED") {
+			  		$scope.showMessage("User "+$scope.username+" was not authorized.");
+			  		$scope.loggedIn=false;
+			  	}
+			  	else if(data.code=="OK") {
+			  		$scope.showMessage("Welcome back "+$scope.username+"!");
+			  		$scope.loggedIn=true;
+			  		
+			  	}
 			  }).
 			  error(function(data, status, headers, config) {
 			   console.log(data);
