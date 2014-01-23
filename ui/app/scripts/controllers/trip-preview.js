675c2e8f8eb0b51eb877d93f9fec845a67bc93e9
zdanowiczkonrad <zdanowiczkonrad@gmail.com>
2014-01-24 00:05:33 +0100

    Added nice icons and displaying POIs

--- ui/app/scripts/controllers/trip-preview.js
+++ ui/app/scripts/controllers/trip-preview.js
@@ -8,6 +8,7 @@ angular.module('uiApp').controller('TripPreviewController', function ($scope,$ht
 	$scope.markers=[];
 	$scope.directions=[];
 	$scope.poiNumber=0;
+	$scope.pois=[];
 
 	var mapOptions = {
           center: new google.maps.LatLng(50.,20.),
@@ -19,6 +20,25 @@ angular.module('uiApp').controller('TripPreviewController', function ($scope,$ht
     	$scope.directions=[];
     },
 
+    $scope.getIconFor = function(type) {
+    	if(type=="Hotel") return 'images/pin_4.png';
+    	else if(type=="Destination") return 'images/pin_3.png';
+    	else if(type=="POI") return 'images/pin_5.png';
+    	else return 'images/pin_3.png';
+    }
+
+    $scope.prepareTooltipFor = function(marker,point) {
+    	var popupContent;
+    	if(point.type=="Hotel") popupContent='<strong><i class="icon-home"></i> Hotel</strong><br>'+point.name;
+    	else if(point.type=="Destination") popupContent='<strong><i class="icon-road"></i> Destination</strong><br/>'+point.name;
+
+    	google.maps.event.addListener(marker, 'click', function() {
+		    infowindow.open($scope.map,marker);
+		  });
+		var infowindow = new google.maps.InfoWindow({
+      		content: popupContent
+ 		 });
+    }
 	$scope.mapShownEvent = function(event,routeData) {
 		clearFields();
 		var scope = angular.element($("#trip-preview")).scope();
@@ -32,11 +52,14 @@ angular.module('uiApp').controller('TripPreviewController', function ($scope,$ht
        // draw points
 
         for(var i=0;i<scope.routeData.points.length;i++) {
-        	new google.maps.Marker({
+        	var marker=new google.maps.Marker({
     			position:new google.maps.LatLng(parseFloat(scope.routeData.points[i].lat), parseFloat(scope.routeData.points[i].lng)),
    		 		map: scope.map,
+   		 		icon: scope.getIconFor(scope.routeData.points[i].type),
     			title:scope.routeData.points[i].name
 			});
+
+			scope.prepareTooltipFor(marker,scope.routeData.points[i]);
 	
         }
 
@@ -110,7 +133,50 @@ angular.module('uiApp').controller('TripPreviewController', function ($scope,$ht
 	}
 
 	$scope.humanize = function(text) {
-		if(text)
-		return text.split("|");
+		if(text) return text.split("|");
+	}
+
+	$scope.showPois=0;
+	$scope.loadPoiMarkers = function() {
+	$scope.routeData.pois.forEach(function(poi) {
+
+    		$http.get(endpoints.be+"/places/poi/"+poi.id).success(function(data, status, headers, config) {
+				$scope.pois.push($scope.getPoiMarker(data));
+				console.log($scope.pois);
+				
+			}).
+			error(function(data, status, headers, config) {
+				console.log("There was an error connecting to the endpoint. is the backend server running on port :9000?");
+			});
+		});
+	}
+
+	$scope.togglePois = function() {
+		$scope.routeData.pois.forEach(function(poi) {
+
+    		$http.get(endpoints.be+"/places/poi/"+poi.id).success(function(data, status, headers, config) {
+				$scope.showPoiMarker(data);
+				console.log(data);
+			}).
+			error(function(data, status, headers, config) {
+				console.log("There was an error connecting to the endpoint. is the backend server running on port :9000?");
+			});
+		});
+	}
+
+	$scope.showPoiMarker = function(poi) {
+
+		var marker = new google.maps.Marker({
+		    map:$scope.map,
+		    animation: google.maps.Animation.DROP,
+		    position: new google.maps.LatLng(parseFloat(poi.latt), parseFloat(poi.longn)),
+		    icon: 'images/pin_5.png'
+  		});
+		google.maps.event.addListener(marker, 'click', function() {
+		    infowindow.open($scope.map,marker);
+		  });
+		var infowindow = new google.maps.InfoWindow({
+      		content: '<i class="icon-star"></i><strong> POI</strong><br/>'+poi.value
+ 		 });
 	}
 });
